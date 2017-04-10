a3f9a6a2c6a3ae56c25efc9b11a6f91b8ba2a524
Frode Sjovatsen <frode@sjovatsen.net>
2017-04-10 23:51:23 +0200

    added javadocs for eventutil

--- src/main/java/no/fint/event/model/EventUtil.java
+++ src/main/java/no/fint/event/model/EventUtil.java
@@ -7,9 +7,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Methods for converting events.
+ */
 public enum EventUtil {
     ;
 
+    /**
+     *
+     * @param json JSON string
+     * @return {@link #Event}
+     */
     public static Event toEvent(String json) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -19,7 +27,13 @@ public enum EventUtil {
         }
     }
 
-
+    /**
+     *
+     * @param event The Event to convert data from.
+     * @param type Type of data
+     * @param <T> Type of data
+     * @return A list of objects of the type specified.
+     */
     public static <T> List<T> convertEventData(Event event, Class<T> type) {
         ObjectMapper objectMapper = new ObjectMapper();
         List<?> dataList = event.getData();
@@ -30,6 +44,11 @@ public enum EventUtil {
     }
 
 
+    /**
+     *
+     * @param event Event object
+     * @return Event object as a JSON string.
+     */
     public static String toJson(Event event) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
