6f9a011c25945aa00021b3b887ecc0d9be66f5e9
jarlehansen <jarle@jarlehansen.net>
2017-06-15 20:58:30 +0200

    added HeaderConstants, updated to fint-buildscripts v1.0.14

---
+++ src/main/java/no/fint/event/model/HeaderConstants.java
@@ -0,0 +1,17 @@
+package no.fint.event.model;
+
+public enum HeaderConstants {
+    ;
+
+    /**
+     * Request header containing organisation id
+     */
+    public static final String ORG_ID = "x-org-id";
+
+
+    /**
+     * Request header containing client
+     */
+    public static final String CLIENT = "x-client";
+
+}
