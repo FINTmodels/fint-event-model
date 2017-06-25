e27da3d14b76304a6e47d5b8273dc9668d0e78f2
jarlehansen <jarle@jarlehansen.net>
2017-06-25 19:41:26 +0200

    added enum values for HealthStatus, received/sent from consumer/provider

--- src/main/java/no/fint/event/model/health/HealthStatus.java
+++ src/main/java/no/fint/event/model/health/HealthStatus.java
@@ -2,6 +2,36 @@ package no.fint.event.model.health;
 
 public enum HealthStatus {
     /**
+     * The health check has been sent from the consumer to the provider
+     */
+    SENT_FROM_CONSUMER_TO_PROVIDER,
+
+    /**
+     * The health check was received in the provider from the consumer
+     */
+    RECEIVED_IN_PROVIDER_FROM_CONSUMER,
+
+    /**
+     * The health check was sent from the provider to the adapter
+     */
+    SENT_FROM_PROVIDER_TO_ADAPTER,
+
+    /**
+     * The health check was received in provider from the adapter
+     */
+    RECEIVED_IN_PROVIDER_FROM_ADAPTER,
+
+    /**
+     * The health check was sent from the provider to the consumer
+     */
+    SENT_FROM_PROVIDER_TO_CONSUMER,
+
+    /**
+     *The health check was received in the consumer from the provider
+     */
+    RECEIVED_IN_CONSUMER_FROM_PROVIDER,
+
+    /**
      * Indicates that the adapter can communicate with the application and everything should work as expected
      */
     APPLICATION_HEALTHY,
