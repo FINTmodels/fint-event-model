6e837ba1667199f85277a7f7370949d455689578
Hknots <hknots99@gmail.com>
2025-05-19 09:50:29 +0200

    feat: Add ODataQueryFilter class to mask OData filter parameters

---
+++ src/main/java/no/fint/event/util/ODataQueryFilter.java
@@ -0,0 +1,15 @@
+package no.fint.event.util;
+
+import java.util.regex.Pattern;
+
+public final class ODataQueryFilter {
+    private ODataQueryFilter() {}
+
+    private static final Pattern ODATA_FILTER =
+            Pattern.compile("(?i)([?&])(\\$filter=)([^&]*)");
+
+    public static String maskODataFilter(String q) {
+        if (q == null) return null;
+        return ODATA_FILTER.matcher(q).replaceAll("$1$2***");
+    }
+}
